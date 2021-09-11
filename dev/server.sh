b9dcf1fc4e6a24bcd234b9dc9fd02e98c431c906
alingse <alingse@foxmail.com>
2021-09-11 19:29:52 +0800

    add dev.sh

---
+++ dev/server.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+bash env.sh
+
+docker run -ti \
+--name wechaty_puppet_service_token_gateway \
+--rm \
+-e WECHATY_LOG \
+-e WECHATY_PUPPET \
+-e WECHATY_PUPPET_SERVER_PORT \
+-e WECHATY_TOKEN \
+-p "$WECHATY_PUPPET_SERVER_PORT:$WECHATY_PUPPET_SERVER_PORT" \
+wechaty/wechaty:latest
